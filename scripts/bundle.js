adf47c58e5ab16fb62981dc87b4292858d79072f
Whitney Murray <wnksmith92@yahoo.com>
2015-10-16 18:29:00 -0500

    all validations work except for success message

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -2,16 +2,16 @@
 'use strict';
 
 $(document).on('ready', function () {
-	var $bigBox = $('.formBox');
+	var $formBox = $('.formBox');
 	var $button = $('.submit');
-	var $nameBox = $('.nameInput');
+	var $nameInput = $('.nameInput');
 	var $nameError = $('.nameError');
-	var $emailBox = $('.emailInput');
+	var $emailInput = $('.emailInput');
 	var $emailError = $('.emailError');
-	var $webBox = $('.webInput');
+	var $webInput = $('.webInput');
 	var $webError = $('.webError');
-	var $tattoo = $('.messInput');
-	var $tattooError = $('.messError');
+	var $messInput = $('.messInput');
+	var $messError = $('.messError');
 	var $successBox = $('.successBox');
 
 	$button.hover(function (e) {
@@ -25,17 +25,17 @@ $(document).on('ready', function () {
 		if ($nameInput.val() === '') {
 			console.log('check');
 			$nameError.html('Name cannot be left empty');
-		} else if ($emailInput.val === '') {
+		} else if ($emailInput.val() === ('' || !"@")) {
 			$emailError.html('Email must contain an \'@\'.');
-		} else if ($webBox.val === '') {
+		} else if ($webInput.val === '') {
 			$webError.html('Website must contain an http://');
-		} else if ($messInput.val === '') {
+		} else if ($messInput.val() === '') {
 			$messError.html('Messages cannot be left empty');
 		} else {
 			console.log('check');
 			$formBox.css({ display: 'none' });
 			$successBox.css({ display: 'block' });
-			$successBox.append('Thank you for contacting us ' + $nameInputval() + '. We have received your message and will touch you shortly.');
+			$successBox.html('Thank you for contacting us ' + $nameInput.val() + '. We have received your message and will touch you shortly.');
 		}
 	});
 });
